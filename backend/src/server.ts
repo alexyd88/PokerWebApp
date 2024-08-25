17cb873fa6f73895d62335563b83ee89b2dcaf52
Alex Du <92554149+alexyd88@users.noreply.github.com>
2024-08-24 22:29:10 -0700

    turn off cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -51,12 +51,11 @@ import {
 } from "game_logic";
 let lobbies = new Map<string, LobbyServer>();
 const SOCKET_PORT = 8080;
-const FRONTEND_ORIGIN = "https://alexyd88.github.io/";
 
 const server = createServer();
 const io = new Server(server, {
   cors: {
-    origin: FRONTEND_ORIGIN,
+    origin: "*",
   },
 });
 
